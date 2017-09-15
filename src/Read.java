a860c3c35876fcb06150e954496bdeb962eefb91
msukan <31421218+msukan@users.noreply.github.com>
2017-09-15 13:03:40 -0500

    Update Read.java

--- src/Read.java
+++ src/Read.java
@@ -1,53 +1,57 @@
 
-
-import java.io.*;
+Import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
+
 public class Read
+
 {
+
 String file1;
+
 public String name[];
 
 public Scanner a;
-    
+
 public void Open()
+
 {
+
 file1 = JOptionPane.showInputDialog("Enter the file name:");
 
 
+
 try
+
 {
-Scanner a= new Scanner(new File(file1));
+(FileReader a= new FileReader(File1));
+int y = a.read();
+System.out.print(char) y;
+y = a.read();
+
+ 
 System.out.println("File found");
+
 }
+
 
    catch(FileNotFoundException e)
+
    {
+
     System.out.println("File Not Found");
+
    }
+
 
    catch(Exception e)
+
    {
+
     System.out.println("Error");
+
    }
-
-
 }
-
-
-public void Read()
-
-{
-int x = 0;
-   while(a.next() != null)
-   {
-    name[x] = a.next();
-    x++;
-   }
-
-
 }
-
-
 }
