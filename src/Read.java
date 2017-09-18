94b8bae7d2abffda1c44b05828c27b8695a0e68b
msukan <31421218+msukan@users.noreply.github.com>
2017-09-18 16:09:00 -0500

    Update Read.java

--- src/Read.java
+++ src/Read.java
@@ -7,58 +7,82 @@ public class Read
 
 {
     
-    String file1;
+    public String file1;
     
     public String name[];
     
     public Scanner a;
     
+   
+    
     public void Open()
     
     {
         
-        file1 = JOptionPane.showInputDialog("Enter the file name:");
+       file1 = JOptionPane.showInputDialog("Enter the file name: (Enter x to exit)");
+     
+       if (file1 == "x")
+       {
+       System.exit(0);	
+       }
         
+       int i=0;
+       int[] word = new int[100];
         
-        
-        try (FileReader a= new FileReader(file1))
+       try (FileReader a= new FileReader(file1))
         
         {
-            System.out.println("File found");
+            System.out.println("\nFile found");
+
+            int y = a.read();   // Reads line in ASCII 
+           
+            while(a.ready())
+            {
+             
+           	word[i]= y;  // Stored as int
+            System.out.print((char)word[i]); // Prints as char
+            i++;
             
-            int y = a.read();
-            while(a.ready()){
-                System.out.print((char) y);
-                y = a.read();
-            }
-            
-            
-            
+            y = a.read(); // Read next line
+                
+            }    
         }
         
         
         catch(FileNotFoundException e)
         
         {
-            
-            System.out.println("File Not Found");
-            
+         System.out.println("File Not Found");
         }
         
         
         catch(Exception e)
         
-        {
-            
-            System.out.println("Error");
-            
+        { 
+         System.out.println("Error");   
         }
+        
     }
     
-    public static void main(String[] args){
-        Read newfile = new Read();
+    public static void main(String[] args)
+    {
+    	String files;
+ 
+        Read newfile= new Read();
         newfile.Open();
+      
+        files = JOptionPane.showInputDialog("More Files?  (Y/N)");
+     
+        if (files.equals("Y") ||  (files.equals("y")))  
+        {
+        Read newfiles = new Read();
+        newfiles.Open();
+        }
+        
+        else if (files.equals("N")  ||  (files.equals("n")) ) 
+        {	
+        System.exit(0);		
+        }
+    
     }
 }
-
-
