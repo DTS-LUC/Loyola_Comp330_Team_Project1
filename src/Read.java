d660a7fefcb54c36097817a491cb61dabc9e848e
msukan <31421218+msukan@users.noreply.github.com>
2017-09-19 15:07:48 -0500

    Update Read.java

--- src/Read.java
+++ src/Read.java
@@ -1,76 +1,56 @@
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
-
+ 
 
 public class Read
-
 {
     
     public String file1;
-    
-    public String name[];
-    
-    public Scanner a;
-    
-   
-    
-    public void Open()
+    public String Open()
     
     {
-        
-       file1 = JOptionPane.showInputDialog("Enter the file name: (Enter x to exit)");
+    file1 = JOptionPane.showInputDialog("Enter the file name: (Enter x to exit)");
      
        if (file1 == "x")
        {
        System.exit(0);	
        }
-        
-       int i=0;
-       int[] word = new int[100];
-        
-       try (FileReader a= new FileReader(file1))
-        
-        {
-            System.out.println("\nFile found");
-
-            int y = a.read();   // Reads line in ASCII 
-           
-            while(a.ready())
-            {
+       
+          FileInputStream in= null;
+          BufferedReader reader = null;
+          String line = null;
              
-           	word[i]= y;  // Stored as int
-            System.out.print((char)word[i]); // Prints as char
-            i++;
-            
-            y = a.read(); // Read next line
-                
-            }    
-        }
-        
-        
+        try { 
+             	   
+         in = new FileInputStream(file1);
+         
+         reader = new BufferedReader(new InputStreamReader(in));
+         
+         for (line = reader.readLine(); line != null; line = reader.readLine())
+         {      
+         System.out.println(line);  
+         }  
+            }   
+   
         catch(FileNotFoundException e)
-        
         {
          System.out.println("File Not Found");
         }
         
-        
         catch(Exception e)
-        
         { 
          System.out.println("Error");   
-        }
-        
+        } 
+       return line;    
     }
     
     public static void main(String[] args)
     {
     	String files;
- 
-        Read newfile= new Read();
+        Read newfile = new Read();
         newfile.Open();
-      
+        
         files = JOptionPane.showInputDialog("More Files?  (Y/N)");
      
         if (files.equals("Y") ||  (files.equals("y")))  
@@ -83,6 +63,5 @@ public class Read
         {	
         System.exit(0);		
         }
-    
     }
 }
