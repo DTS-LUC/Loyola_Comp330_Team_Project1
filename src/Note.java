86a21b81f78a397a20eba45c28d05ae76608439f
donald-stolz <donaldtstolz@gmail.com>
2017-09-19 10:18:16 -0500

    Add comments/TODOs on Note class. Created NoteSorter to use main

---
+++ src/Note.java
@@ -0,0 +1,88 @@
+public class Note {
+
+  ArrayList<String> mentions = new ArrayList<String>(); // '@'
+  ArrayList<String> topics = new ArrayList<String>();   // '#'
+  ArrayList<String> urls = new ArrayList<String>();     // ''
+
+  Private String id;  // '!'
+  // TODO: references; Most likely using nodes
+
+  // TODO: Add constructor
+
+  // TODO: create Class methods
+
+  // Recieves an Arraylist to set mentions
+  public void setMentions(ArrayList mentions){
+
+    }
+
+  // Returns an ArrayList of all mentions
+  public getMentions(){
+
+    }
+
+  // Returns index of first meniton if a match is located; Else return -1
+  public int findMention(){
+
+    }
+
+  // Recieves an Arraylist to set topics
+  public void setTopics(ArrayList topics){
+
+    }
+
+  // Returns an ArrayList of all topics
+  public getTopics(){
+
+    }
+
+  // Returns index of first topic if a match is located; Else return -1
+  public int findTopic(){
+
+    }
+
+  // Recieves an Arraylist to set urls
+  public void setURLs(ArrayList urls){
+
+    }
+
+  // Returns an ArrayList of all urls
+  public getURLs(){
+
+    }
+
+  // Returns index of first URL if a match is located; Else return -1
+  public int findURL(){
+
+    }
+
+
+
+
+  public getID(){
+
+    }
+
+  public void setID(String id){
+
+    }
+
+
+  // public getRefs(){
+  //
+  //   }
+  //
+  // public void setRefs(ArrayList refs){
+  //
+  //   }
+
+
+  public String getText(){
+
+    }
+
+  public void setText(){
+
+    }
+
+}
