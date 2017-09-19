de4151b0149e755137648a8c62d6bd8cbdb56975
msukan <31421218+msukan@users.noreply.github.com>
2017-09-19 12:20:54 -0500

    Update NoteSorter.java

--- src/NoteSorter.java
+++ src/NoteSorter.java
@@ -4,11 +4,13 @@ public class NoteSorter{
 
   public static void main(String[] args) {
     // First: Select directory to upload files
+    // Array of Strings
 
     // Second: Parse files with Regex
 
     // Third: Store fields in a note object
+    // Array of notes
 
-    // Fourth: 
+    // Fourth: User Interface
   }
 }
