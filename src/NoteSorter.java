f36fc05fa430e3eb342fd3174ff99256a5f358a8
donald-stolz <donaldtstolz@gmail.com>
2017-09-19 11:56:13 -0500

    Added some comments to explain during meeting today

---
+++ src/NoteSorter.java
@@ -0,0 +1,14 @@
+
+
+public class NoteSorter{
+
+  public static void main(String[] args) {
+    // First: Select directory to upload files
+
+    // Second: Parse files with Regex
+
+    // Third: Store fields in a note object
+
+    // Fourth: 
+  }
+}
