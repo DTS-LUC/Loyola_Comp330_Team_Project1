a484d626394b2afad5fbcf8303dd00d1f1697bc2
msukan <31421218+msukan@users.noreply.github.com>
2017-09-20 13:28:56 -0500

    Update Read.java

--- src/Read.java
+++ src/Read.java
@@ -1,17 +1,57 @@
 import java.io.*;
+import java.awt.*;
 import java.util.*;
 import javax.swing.*;
  
 
-public class Read
+public class Read  
 {
     
-    public String file1;
-    public String Open()
+	
+	String files;
+   
+    String folder,file1;
     
-    {
-    file1 = JOptionPane.showInputDialog("Enter the file name: (Enter x to exit)");
+         Desktop desktop = Desktop.getDesktop();
+         File folderOpen = null;
+        
+ String folders()
+ {
+ 	 
+ 	          folder = JOptionPane.showInputDialog("Enter folder name");
+        
+        try 
+        
+        {	
+           folderOpen = new File(folder);
+            desktop.open(folderOpen);
+         
+        } 
+        
+        
+        catch (IllegalArgumentException i)
+        {
+            System.out.println("File Not Found");
+        }
+       catch(Exception i)
+      { 
+       System.out.println("Error");   
+        } 
+  	  
+ 	  
+ 	 return null;
+ 	 
+ }
+    
+    
+  String Open()
+  {
+  	  
+  	 
+    file1 = JOptionPane.showInputDialog("Enter the directory then file name: (Enter x to exit)", folder + "/");
      
+    
+    
        if (file1 == "x")
        {
        System.exit(0);	
@@ -20,42 +60,56 @@ public class Read
           FileInputStream in= null;
           BufferedReader reader = null;
           String line = null;
+          String mline = "";
              
         try { 
              	   
          in = new FileInputStream(file1);
          
+         
          reader = new BufferedReader(new InputStreamReader(in));
          
          for (line = reader.readLine(); line != null; line = reader.readLine())
          {      
-         System.out.println(line);  
+       
+         mline = mline.concat(" " + line);
          }  
+           System.out.println(mline); 
+         return mline;  
+         
             }   
    
-        catch(FileNotFoundException e)
-        {
-         System.out.println("File Not Found");
-        }
+         catch(FileNotFoundException e)
+         {
+          System.out.println("File Not Found1");
+         }
         
-        catch(Exception e)
-        { 
-         System.out.println("Error");   
-        } 
-       return line;    
-    }
+         catch(Exception e)
+         { 
+          System.out.println("Error");   
+         } 
+          return null;
+   
+   }
     
     public static void main(String[] args)
     {
-    	String files;
-        Read newfile = new Read();
-        newfile.Open();
+       String files= "Y";
+       
+       Read newfile = new Read();
+       newfile.folders();
+       newfile.Open();
         
+       
+       while ((files != "N") || (files != "n"))
+       {
         files = JOptionPane.showInputDialog("More Files?  (Y/N)");
      
         if (files.equals("Y") ||  (files.equals("y")))  
         {
         Read newfiles = new Read();
+        
+       
         newfiles.Open();
         }
         
@@ -63,5 +117,6 @@ public class Read
         {	
         System.exit(0);		
         }
-    }
+       }  
+     }
 }
