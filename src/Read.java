846d3ec84d52604c57bb3594f17ad7e72280a4c7
Donald Stolz <donaldtstolz@gmail.com>
2017-09-20 21:36:48 -0500

    Changed Read file to read directory

--- src/Read.java
+++ src/Read.java
@@ -1,67 +1,23 @@
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
- 
+
 
 public class Read
 {
-    
-    public String file1;
-    public String Open()
-    
-    {
-    file1 = JOptionPane.showInputDialog("Enter the file name: (Enter x to exit)");
-     
-       if (file1 == "x")
-       {
-       System.exit(0);	
-       }
-       
-          FileInputStream in= null;
-          BufferedReader reader = null;
-          String line = null;
-             
-        try { 
-             	   
-         in = new FileInputStream(file1);
-         
-         reader = new BufferedReader(new InputStreamReader(in));
-         
-         for (line = reader.readLine(); line != null; line = reader.readLine())
-         {      
-         System.out.println(line);  
-         }  
-            }   
-   
-        catch(FileNotFoundException e)
-        {
-         System.out.println("File Not Found");
-        }
-        
-        catch(Exception e)
-        { 
-         System.out.println("Error");   
-        } 
-       return line;    
+    public static void listFilesForFolder(final File folder) {
+      for (final File fileEntry : folder.listFiles()) {
+          if (fileEntry.isDirectory()) {
+              listFilesForFolder(fileEntry);
+          } else {
+              System.out.println(fileEntry.getName());
+          }
+      }
     }
-    
+
     public static void main(String[] args)
     {
-    	String files;
-        Read newfile = new Read();
-        newfile.Open();
-        
-        files = JOptionPane.showInputDialog("More Files?  (Y/N)");
-     
-        if (files.equals("Y") ||  (files.equals("y")))  
-        {
-        Read newfiles = new Read();
-        newfiles.Open();
-        }
-        
-        else if (files.equals("N")  ||  (files.equals("n")) ) 
-        {	
-        System.exit(0);		
-        }
+      final File folder = new File("/Users/don/Desktop");
+      listFilesForFolder(folder);
     }
 }
