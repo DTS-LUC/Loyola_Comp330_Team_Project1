4769904ef7c93929e02943dbcdcfd0cd73a04357
Donald Stolz <donaldtstolz@gmail.com>
2017-09-21 10:43:30 -0500

    Completed setMentions method for NoteSorter

--- src/NoteSorter.java
+++ src/NoteSorter.java
@@ -3,15 +3,39 @@ import java.util.*;
 
 public class NoteSorter{
 
-	public sortMentions TreeMap<String,ArrayList<String>>(TreeMap<String,ArrayList<String>> files){
-		// Key: @identifier Value: fileName
+	public sortMentions TreeMap<String,ArrayList<String>>(TreeMap<String,String> files){
+		// TreeMap store list of all mentions
+		// Key: @identifier Value: fileName(s);
+		// Key: String			Value: Arraylist to dynamically add Strings
 		TreeMap<String,ArrayList<String>> mentions = new TreeMap<String,ArrayList<String>>(); // '@'
-		// Iterate through text files
-		// For each file run regexChecker to get identifiers
-			// Check to see if key already exists
-					/*True: Copy value to temp list; Add to list;
-										put key with new value*/
-					//False: Add key value pair
+
+		ArrayList<String> mentionKeys = new ArrayList<String>();
+		ArrayList<String> temp = new ArrayList<String>();
+
+		String fileName;
+		// Loop through files
+		for(Map.Entry<String,String> file : files.entrySet()) {
+  		fileName = file.getKey();
+			//Get all mentions from the file text with regexChecker
+			mentionKeys = regexChecker("@[a-zA-Z_][0-9a-zA-Z_]*", file.getValue());
+			// Add mention keys to mentions TreeMap
+			for(String m:mentionKeys) {
+				// check if value exists
+				if (mentions.containsKey(m)){
+					// True: Add to ArrayList
+					temp = mentions.get(m);
+					temp.add(fileName);
+					mentions.put(m, temp);
+				}
+				else {
+					// False: Add key value
+					temp.add(fileName);
+					mentions.put(m, temp);
+				}
+				temp.clear();
+			}
+		}
+
 		return mentions;
 	}
 
@@ -27,11 +51,10 @@ public class NoteSorter{
 				matches.add(regexMatcher.group().trim());
 			}
 		}
-
-		for(String m:matches) {
-			System.out.println(m);
-		}
-		System.out.println(matches);
+		// for(String m:matches) {
+		// 	System.out.println(m);
+		// }
+		// System.out.println(matches);
 		return matches;
 	}
 }
