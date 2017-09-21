5bd2cf253f44261735f947a92e03574c01a70bd7
Donald Stolz <donaldtstolz@gmail.com>
2017-09-21 18:23:01 -0500

    Started building command line interface

--- src/NoteTakerApp.java
+++ src/NoteTakerApp.java
@@ -12,20 +12,71 @@ public class NoteTakerApp{
 	}
 
 	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		boolean runApp = true;
+
+
 		NoteRetriever retriever = new NoteRetriever();
 		NoteSorter		sorter		= new NoteSorter();
+		Notes					notes			= new Notes();
 		HashMap<String, String> files = new HashMap<String, String>();
 
 		//Get directory path from user
-		String dirPath = "/Users/don/Loyola_Comp330_Team_Project1/TestNotes";
+		System.out.println("Enter directory path:");
+		String dirPath = input.nextLine();
+		dirPath = "/Users/don/Loyola_Comp330_Team_Project1/TestNotes";
+
 		// Send directory path to NoteRetriever
 		files = retriever.getFiles(dirPath);
 
-		printTree(sorter.sortMentions(files));
-		// Send files to Notes
-		// Notes notes = new Notes(files);
+		// Set values for notes
+		notes.setMentions(sorter.sort(files, "@[a-zA-Z_][0-9a-zA-Z_]*"));
+		notes.setTopics(sorter.sort(files, "#[a-zA-Z_][0-9a-zA-Z_]*"));
+
+		//Command Line Interface
+		ArrayList<String> listArgs;
+		String userArgs;
+		int numArgs;
+		while(runApp){
+			System.out.println("Enter a command or \"Q/q\" to quit:");
+			userArgs = input.nextLine();
+
+			listArgs = new ArrayList<String>(Arrays.asList(userArgs.split(" ")));
+			numArgs = listArgs.size();
+
+			if (numArgs == 1) {
+				// System.out.println(listArgs.get(0));
+				switch (listArgs.get(0)){
+					case "@" :
+						printTree(notes.getAllMentions());
+						break;
+					case "#" :
+						printTree(notes.getAllTopics());
+						break;
+					case "Q" :
+					case "q" :
+						runApp = false;
+						break;
+					default:
+						System.out.println("Please enter a valid command");
+				}
+			}
+			else if (numArgs > 1) {
+				//Call selectively
+			}
+			else{
+				runApp = false;
+			}
+		}
+
+
 		// Call methods in Notes
 		// Swtich cases match character from input[0]
 
 	}
 }
+
+// printTree(notes.getAllMentions());
+// printTree(notes.getAllTopics());
+// printTree(sorter.sort(files, "\\^[a-zA-Z_][0-9a-zA-Z_]*"));
